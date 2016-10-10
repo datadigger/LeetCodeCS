37fca067215ced5fce3ebb2bacf46452d7cef247
yongqiang <jasonleehust@gmail.com>
2016-10-10 00:38:27 -0700

    Resolve more issues.

---
+++ LeetCode/PalindromeNumber.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode
+{
+    public class PalindromeNumber
+    {
+        public bool IsPalindrome(int x)
+        {
+            if (x < 0)
+            {
+                return false;
+            }
+
+            int p = 1;
+            while (x / p >= 10)
+            {
+                p *= 10;
+            }
+
+            while (x > 0) 
+            {
+                if (x / p != x % 10)
+                {
+                    return false;
+                }
+
+                x -= (x % 10) * p;
+                x /= 10;
+                p /= 100;
+            }
+
+            return true;
+        }
+    }
+}
---
+++ LeetCodeUnitTest/PalindromeNumberTest.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace LeetCodeUnitTest
+{
+    using LeetCode;
+
+    [TestClass]
+    class PalindromeNumberTest
+    {
+        private PalindromeNumber util = new PalindromeNumber();
+
+        [TestMethod]
+        public void testSimple()
+        {
+           Assert.IsTrue(this.util.IsPalindrome(121)); 
+           Assert.IsTrue(this.util.IsPalindrome(1221)); 
+           Assert.IsTrue(this.util.IsPalindrome(111)); 
+           Assert.IsTrue(this.util.IsPalindrome(1111)); 
+       
+           Assert.IsTrue(this.util.IsPalindrome(1212)); 
+           Assert.IsFalse(this.util.IsPalindrome(123)); 
+
+           
+           Assert.IsFalse(this.util.IsPalindrome(321)); 
+           Assert.IsFalse(this.util.IsPalindrome(10)); 
+        }
+
+    }
+}
