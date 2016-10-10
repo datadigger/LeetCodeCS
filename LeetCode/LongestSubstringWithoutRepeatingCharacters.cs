ac404e3bf10232a33b8ba9a8ad86ff5556f29134
yongqiang <jasonleehust@gmail.com>
2016-10-09 23:44:42 -0700

    Init commit

---
+++ LeetCode/LongestSubstringWithoutRepeatingCharacters.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode
+{
+    public class LongestSubstringWithoutRepeatingCharacters
+    {
+        public int LengthOfLongestSubstring(string s)
+        {
+            if (s.Length == 1)
+            {
+                return 1;
+            }
+
+            int maxLength = 0;
+
+            int p = 0, q = 0;
+
+            IDictionary<char, int> usedChars = new Dictionary<char, int>();
+
+            while (q < s.Length)
+            {
+                var c = s[q];
+                if (usedChars.ContainsKey(c))
+                {
+                    var oldIndex = usedChars[c];
+
+                    for (int i = p; i <= oldIndex; i++)
+                    {
+                        usedChars.Remove(s[i]);
+                    }
+
+                    usedChars.Add(c, q);
+                    p = oldIndex + 1;
+                }
+                else
+                {
+                    usedChars.Add(c, q);
+                }
+
+                ++q;
+                maxLength = Math.Max(maxLength, q - p);
+            }
+
+            return maxLength;
+        }
+    }
+}
---
+++ LeetCodeUnitTest/LongestSubstringWithoutRepeatingCharactersTest.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using LeetCode;
+
+namespace LeetCodeUnitTest
+{
+    [TestClass]
+    public class LongestSubstringWithoutRepeatingCharactersTest
+    {
+        private LongestSubstringWithoutRepeatingCharacters util = new LongestSubstringWithoutRepeatingCharacters();
+
+        [TestMethod]
+        public void TestSimple()
+        {
+           Assert.AreEqual(3, this.util.LengthOfLongestSubstring("abcabcbb"));
+           Assert.AreEqual(4, this.util.LengthOfLongestSubstring("abcabcdbb"));
+           Assert.AreEqual(1, this.util.LengthOfLongestSubstring("bbbbb"));
+           Assert.AreEqual(3, this.util.LengthOfLongestSubstring("pwwkew"));
+           Assert.AreEqual(1, this.util.LengthOfLongestSubstring("c"));
+           Assert.AreEqual(1, this.util.LengthOfLongestSubstring("cc"));
+           Assert.AreEqual(2, this.util.LengthOfLongestSubstring("ca"));
+        }
+    }
+}
