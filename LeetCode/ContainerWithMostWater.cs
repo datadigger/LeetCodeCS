b026e1e9e3c23ba474fbdc70b8d2a122837f585c
yongqiang <jasonleehust@gmail.com>
2016-10-16 18:55:12 -0700

    Resolve ContainerWithMostWater

---
+++ LeetCode/ContainerWithMostWater.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode
+{
+    public class ContainerWithMostWater
+    {
+        public int MaxArea(int[] height)
+        {
+            int li = 0, ri = height.Length - 1;
+
+            int ans = (ri - li) * Math.Min(height[li], height[ri]);
+
+            while (li < ri)
+            {
+                if (height[li] < height[ri])
+                {
+                    ++li;
+                }
+                else
+                {
+                    --ri;
+                }
+
+                ans = Math.Max(ans, (ri - li) * Math.Min(height[li], height[ri]));
+            }
+
+
+            return ans;
+        }
+    }
+}
---
+++ LeetCodeUnitTest/ContainerWithMostWaterTest.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Text;
+using System.Collections.Generic;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace LeetCodeUnitTest
+{
+    using LeetCode;
+
+    /// <summary>
+    /// Summary description for ContainerWithMostWaterTest
+    /// </summary>
+    [TestClass]
+    public class ContainerWithMostWaterTest
+    {
+        private ContainerWithMostWater util = new ContainerWithMostWater();
+
+        [TestMethod]
+        public void TestSimple()
+        {
+            Assert.AreEqual(1, this.util.MaxArea(new int[] {1, 1}));
+            Assert.AreEqual(10, this.util.MaxArea(new int[] {1, 0, 50, 0, 0, 0, 0, 0, 0, 0, 1}));
+        }
+    }
+}
