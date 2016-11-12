37121f4399cb2b97c1a76957e62bfdf388d974d5
yongqiang <jasonleehust@gmail.com>
2016-11-12 12:49:28 -0800

    Added AddStrings

---
+++ LeetCode/AddStrings.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode
+{
+    public class AddStringsUtil
+    {
+        public string AddStrings(string num1, string num2)
+        {
+            return string.Empty;
+        }
+    }
+}
