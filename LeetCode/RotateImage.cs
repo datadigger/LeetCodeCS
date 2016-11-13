d9b447906b61415571bbdf7cbcf9137d97a0259d
yongqiang <jasonleehust@gmail.com>
2016-11-13 11:12:20 -0800

    Resolve RotateImage (in-place replacement)

---
+++ LeetCode/RotateImage.cs
@@ -0,0 +1,37 @@
+﻿// --------------------------------------------------------------------------------------------------------------------
+// <copyright file="RotateImage.cs" company="">
+//   
+// </copyright>
+// <summary>
+//   Defines the RotateImage type.
+// </summary>
+// --------------------------------------------------------------------------------------------------------------------
+
+using System;
+
+namespace LeetCode
+{
+    public class RotateImage
+    {
+        public void Rotate(int[,] matrix)
+        {
+            int n = (int)Math.Sqrt(matrix.Length);
+
+            for (int i = 0; i < n / 2; ++i)
+            {
+                for (int j = i; j < n - 1 - i; ++j)
+                {
+                    var tmp = matrix[i, j];
+
+                    matrix[i, j] = matrix[n - 1 - j, i];
+
+                    matrix[n - 1 - j, i] = matrix[n - 1 - i, n - 1 - j];
+
+                    matrix[n - 1 - i, n - 1 - j] = matrix[j, n - 1 - i];
+
+                    matrix[j, n - 1 - i] = tmp;
+                }
+            }
+        }
+    }
+}
---
+++ LeetCodeUnitTest/RotateImageTest.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCodeUnitTest
+{
+    using LeetCode;
+
+    using NUnit.Framework;
+
+    [TestFixture]
+    class RotateImageTest
+    {
+        private readonly RotateImage util = new RotateImage();
+
+        [Test]
+        public void TestSimple()
+        {
+            var image1 = new int[,] { { 1, 2, 3 }, { 4, 5, 6 }, { 7, 8, 9 } };
+            var rotatedImage = new int[,] { { 7, 4, 1 }, { 8, 5, 2 }, { 9, 6, 3 } };
+
+            this.util.Rotate(image1);
+            Assert.That(rotatedImage, Is.EqualTo(image1));
+        }
+
+        [Test]
+        public void TestSimple2()
+        {
+            var image = new int[,]
+                            {
+                                    { 1, 2, 3, 4, 5 }, { 6, 7, 8, 9, 10 }, { 11, 12, 13, 14, 15 }, { 16, 17, 18, 19, 20 },
+                                    { 21, 22, 23, 24, 25 }
+                            };
+
+            var rotateImage = new int[,]
+                                  {
+                                          { 21, 16, 11, 6, 1 }, { 22, 17, 12, 7, 2 }, { 23, 18, 13, 8, 3 },
+                                          { 24, 19, 14, 9, 4 }, { 25, 20, 15, 10, 5 }
+                                  };
+
+            this.util.Rotate(image);
+            Assert.That(rotateImage, Is.EqualTo(image));
+        }
+    }
+}
