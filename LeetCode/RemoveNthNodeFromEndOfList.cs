1300f54631f96ef6e923a0a3c57886c0987d801e
yongqiang <jasonleehust@gmail.com>
2016-11-13 23:33:34 -0800

    Resove RemoveNthNodeFromEndoOfList.

---
+++ LeetCode/RemoveNthNodeFromEndOfList.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode
+{
+    public class RemoveNthNodeFromEndOfList
+    {
+        public ListNode RemoveNthFromEnd(ListNode head, int n)
+        {
+            var q = new ListNode(-1);
+            q.next = head;
+
+            var p = q;
+
+            for (int i = 0; i < n; ++i)
+            {
+                head = head.next;
+            }
+
+            while (head != null)
+            {
+                head = head.next;
+                q = q.next;
+            }
+
+            q.next = q.next.next;
+
+            return p.next;
+        }
+    }
+}
---
+++ LeetCodeUnitTest/RemoveNthNodeFromEndOfListTest.cs
@@ -0,0 +1,28 @@
+﻿namespace LeetCodeUnitTest
+{
+    using LeetCode;
+
+    using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+    using Assert = Microsoft.VisualStudio.TestTools.UnitTesting.Assert;
+
+    [TestClass]
+    public class RemoveNthNodeFromEndOfListTest
+    {
+        private readonly RemoveNthNodeFromEndOfList util = new RemoveNthNodeFromEndOfList();
+
+        [TestMethod]
+        public void TestSimple()
+        {
+            Assert.IsTrue(
+                ListNode.Equals(
+                    ListNode.Create(new int[] { 2 }),
+                    this.util.RemoveNthFromEnd(ListNode.Create(new int[] { 1, 2 }), 2)));
+
+            Assert.IsTrue(
+                ListNode.Equals(
+                    ListNode.Create(new int[] {1, 2, 4, 5}),
+                    this.util.RemoveNthFromEnd(ListNode.Create(new int[] { 1, 2, 3, 4, 5 }), 3)));
+        }
+    }
+}
