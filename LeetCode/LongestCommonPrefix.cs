47ccac3dc45275b1c5852a63c36dcd9f8a2d9c2b
yongqiang <jasonleehust@gmail.com>
2016-11-15 00:22:38 -0800

    Resolve LongestCommonPrefix

---
+++ LeetCode/LongestCommonPrefix.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode
+{
+    public class LongestCommonPrefix
+    {
+        public string DoSearch(string[] strs)
+        {
+            if (strs == null || strs.Length == 0)
+            {
+                return string.Empty;
+            }
+
+            if (strs.Length == 1)
+            {
+                return strs[0];
+            }
+
+            var minLength = strs[0].Length;
+            for (var i = 1; i < strs.Length; ++i)
+            {
+                minLength = Math.Min(minLength, strs[i].Length);
+            }
+
+            int prefixIndex = 0;
+
+            for (; prefixIndex < minLength; ++prefixIndex)
+            {
+                var c = strs[0][prefixIndex];
+
+                var isCommon = true;
+                for (var i = 1; i < strs.Length; ++i)
+                {
+                    if (c != strs[i][prefixIndex])
+                    {
+                        isCommon = false;
+                        break;
+                    }
+                }
+
+                if (!isCommon)
+                {
+                    break;
+                }
+            }
+
+            return strs[0].Substring(0, prefixIndex);
+        }
+    }
+}
---
+++ LeetCodeUnitTest/LongestCommonPrefixTest.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCodeUnitTest
+{
+    using LeetCode;
+
+    using NUnit.Framework;
+
+    [TestFixture]
+    public class LongestCommonPrefixTest
+    {
+        private readonly LongestCommonPrefix util = new LongestCommonPrefix();
+
+        [Test]
+        public void TestSimple()
+        {
+            Assert.That("aaa", Is.EqualTo(this.util.DoSearch(new string[] {"aaaa", "aaab", "aaa", "aaac"})));
+            Assert.That(string.Empty, Is.EqualTo(this.util.DoSearch(new string[] {"a", "b"})));
+            Assert.That("abc", Is.EqualTo(this.util.DoSearch(new string[] { "abc"})));
+
+            Assert.That(string.Empty, Is.EqualTo(this.util.DoSearch(new string[] {})));
+        }
+    }
+}
