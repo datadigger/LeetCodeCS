ce71bdce4a02d71b381b05976d8778087516485e
yongqiang <jasonleehust@gmail.com>
2016-11-15 22:11:47 -0800

    Resolve MinMove

---
+++ LeetCode/MinimumMovesToEqualArrayElements.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode
+{
+    public class MinimumMovesToEqualArrayElements
+    {
+        public int MinMoves(int[] nums)
+        {
+            if (nums.Length == 1)
+            {
+                return 0;
+            }
+
+            var min = nums[0];
+
+            for (int i = 1; i < nums.Length; ++i)
+            {
+                min = Math.Min(min, nums[i]);
+            }
+
+            var sum = 0;
+            foreach (var num in nums)
+            {
+                sum += (num - min);
+            }
+
+            return sum;
+        }
+    }
+}
---
+++ LeetCodeUnitTest/MinimumMovesToEqualArrayElementsTest.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCodeUnitTest
+{
+    using LeetCode;
+
+    using NUnit.Framework;
+
+    [TestFixture]
+    class MinimumMovesToEqualArrayElementsTest
+    {
+        private readonly MinimumMovesToEqualArrayElements util = new MinimumMovesToEqualArrayElements();
+
+        [Test]
+        public void TestSimple()
+        {
+            Assert.That(3, Is.EqualTo(this.util.MinMoves(new int[] {1, 2, 3})));
+            Assert.That(7, Is.EqualTo(this.util.MinMoves(new int[] {2, 9})));
+            Assert.That(0, Is.EqualTo(this.util.MinMoves(new int[] {2 })));
+
+        }
+    }
+}
