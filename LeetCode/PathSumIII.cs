0534d4eacb0f3dd4b83a2b6013e5aedc8320a799
yongqiang <jasonleehust@gmail.com>
2016-11-15 23:54:07 -0800

    * LeetCode.csproj: Added some classes
    * TreeNode.cs:
    * PathSumIII.cs:
    * PathSumIIITest.cs:

---
+++ LeetCode/PathSumIII.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace LeetCode
+{
+  public class PathSumIII
+  {
+    public int PathSum(TreeNode root, int sum)
+    {
+      if (root == null) {
+        return 0;
+      }
+
+      int totalPath = 0;
+
+      if (root.val == sum) {
+        ++totalPath;
+      }
+
+      totalPath += PathSum(root.left, sum) + PathSum(root.right, sum);
+      totalPath += PathSum(root.left, sum - root.val) 
+                 + PathSum(root.right, sum - root.val);
+
+      return totalPath;
+    }
+  }
+}
---
+++ LeetCode/testing/PathSumIIITest.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using NUnit.Framework;
+
+namespace LeetCode
+{
+  [TestFixture]
+  public class PathSumIIITest
+  {
+    private readonly PathSumIII util = new PathSumIII();
+    
+    [Test]
+    public void TestSimple() {
+      TreeNode node1 = new TreeNode(10);
+      TreeNode node2 = new TreeNode(5);
+      TreeNode node3 = new TreeNode(-3);
+      TreeNode node4 = new TreeNode(3);
+      TreeNode node5 = new TreeNode(2);
+      TreeNode node6 = new TreeNode(11);
+      TreeNode node7 = new TreeNode(3);
+      TreeNode node8 = new TreeNode(-2);
+      TreeNode node9 = new TreeNode(1);
+
+      node1.left = node2;
+      node1.right = node3;
+
+      node2.left = node4;
+      node2.right = node5;
+
+      node3.right = node6;
+
+      node4.left = node7;
+      node4.right = node8;
+
+      node5.right = node9;
+
+      Assert.That(3, Is.EqualTo(util.PathSum(node1, 8)));
+    }
+  }
+}
