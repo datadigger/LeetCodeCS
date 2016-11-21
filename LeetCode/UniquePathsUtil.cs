c538f55031cad95d97c1ca626c9dce3ba1db8768
Yongqiang Li <jasonleehust@gmail.com>
2016-11-21 08:30:55 -0800

    Resolve Problem of Unique Paths

---
+++ LeetCode/UniquePathsUtil.cs
@@ -0,0 +1,35 @@
+﻿namespace LeetCode
+{
+    public class UniquePathsUtil
+    {
+        public int UniquePaths(int m, int n)
+        {
+            if (m == 0 || n == 0)
+            {
+                return 0;
+            }
+
+            var sum = new int[m, n];
+
+            for (var i = 0; i < m; ++i)
+            {
+                sum[i, 0] = 1;
+            }
+
+            for (var j = 0; j < n; ++j)
+            {
+                sum[0, j] = 1;
+            }
+
+            for (var i = 1; i < m; ++i)
+            {
+                for (var j = 1; j < n; ++j)
+                {
+                    sum[i, j] = sum[i, j - 1] + sum[i - 1, j];
+                }
+            }
+
+            return sum[m - 1, n - 1];
+        }
+    }
+}
---
+++ LeetCode/testing/UniquePathsUtilTest.cs
@@ -0,0 +1,19 @@
+﻿namespace LeetCode
+{
+    using NUnit.Framework;
+
+    [TestFixture]
+    public class UniquePathsUtilTest
+    {
+        private readonly UniquePathsUtil util = new UniquePathsUtil();
+
+        [Test]
+        public void TestSimple()
+        {
+            Assert.That(1, Is.EqualTo(expected: this.util.UniquePaths(1, 1)));
+            Assert.That(2, Is.EqualTo(expected: this.util.UniquePaths(2, 2)));
+            Assert.That(0, Is.EqualTo(expected: this.util.UniquePaths(0, 1)));
+            Assert.That(0, Is.EqualTo(expected: this.util.UniquePaths(2, 0)));
+        }
+    }
+}
