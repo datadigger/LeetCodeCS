f77daf557dfb30562bfbcf2babd1fbe5d54bf7c4
Yongqiang Li <jasonleehust@gmail.com>
2016-11-20 17:29:51 -0800

    Resolve Problem of “Intersection of Two Arrays II”

---
+++ LeetCode/IntersectionOfTwoArraysII.cs
@@ -0,0 +1,37 @@
+﻿namespace LeetCode
+{
+    using System.Collections.Generic;
+
+    public class IntersectionOfTwoArraysII
+    {
+        public int[] Intersect(int[] nums1, int[] nums2)
+        {
+            var aDict = new Dictionary<int, int>();
+
+            foreach (var num in nums1)
+            {
+                if (aDict.ContainsKey(num))
+                {
+                    aDict[num] = aDict[num] + 1;
+                }
+                else
+                {
+                    aDict[num] = 1;
+                }
+            }
+
+            var results = new List<int>();
+
+            foreach (var num in nums2)
+            {
+                if (aDict.ContainsKey(num) && aDict[num] > 0)
+                {
+                    results.Add(num);
+                    aDict[num] = aDict[num] - 1;
+                }
+            }
+
+            return results.ToArray();
+        }
+    }
+}
---
+++ LeetCode/testing/IntersectionOfTwoArraysIITest.cs
@@ -0,0 +1,16 @@
+﻿namespace LeetCode
+{
+    using NUnit.Framework;
+
+    [TestFixture]
+    public class IntersectionOfTwoArraysIITest
+    {
+        private readonly IntersectionOfTwoArraysII util = new IntersectionOfTwoArraysII();
+
+        [Test]
+        public void TestSimple()
+        {
+            Assert.AreEqual(new int[] {2, 2}, this.util.Intersect(new int[] {1, 2, 2, 1}, new int[] {2, 2}));
+        }
+    }
+}
