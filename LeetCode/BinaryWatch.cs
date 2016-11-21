d2277ae2997cde414a650efeaffb3f41c5a4f6e2
Yongqiang Li <jasonleehust@gmail.com>
2016-11-20 17:00:18 -0800

    Resolve problem of BinaryWatch

--- LeetCode/BinaryWatch.cs
+++ LeetCode/BinaryWatch.cs
@@ -1,12 +1,87 @@
 ﻿namespace LeetCode
 {
+    using System;
     using System.Collections.Generic;
+    using System.Runtime.InteropServices;
 
     public class BinaryWatch
     {
         public IList<string> ReadBinaryWatch(int num)
         {
-            return null;
+            var results = new List<string>();
+
+            var bits = new int[10];
+
+            if (num == 0)
+            {
+                results.Add("0:00");
+            }
+            else
+            {
+                var resultSet = new HashSet<string>();
+                for (var i = 0; i < 10; ++i)
+                {
+                    bits[i] = 1;
+                    doReadBinaryWatch(num - 1, bits, resultSet);
+                    bits[i] = 0;
+                }
+
+                foreach (var reading in resultSet)
+                {
+                    results.Add(reading);
+                }
+            }
+
+            return results;
+        }
+
+        private void doReadBinaryWatch(int sum, int[] bits, HashSet<string> results)
+        {
+            if (sum == 0)
+            {
+                var hour = toHour(bits);
+                var min = toMin(bits);
+
+                if (hour < 12 && min < 60)
+                {
+                    results.Add($"{hour}:{min:D2}");
+                }
+                return;
+            }
+
+            for (int i = 0; i < 10; ++i)
+            {
+                if (bits[i] == 1)
+                {
+                    continue;
+                }
+
+                bits[i] = 1;
+                doReadBinaryWatch(sum - 1, bits, results);
+                bits[i] = 0;
+            }
+        }
+
+        private static int toMin(int[] bits)
+        {
+            var min = 0;
+            for (var i = 4; i < 10; ++i)
+            {
+                min *= 2;
+                min += bits[i];
+            }
+            return min;
+        }
+
+        private static int toHour(int[] bits)
+        {
+            var hour = 0;
+            for (var i = 0; i < 4; ++i)
+            {
+                hour *= 2;
+                hour += bits[i];
+            }
+            return hour;
         }
     }
 }
--- LeetCode/testing/BinaryWatchTest.cs
+++ LeetCode/testing/BinaryWatchTest.cs
@@ -1,6 +1,7 @@
 ﻿namespace LeetCode
 {
     using System.Collections.Generic;
+    using System.Runtime;
 
     using NUnit.Framework;
 
@@ -18,6 +19,35 @@
             var set2 = new HashSet<string> { "2", "1" };
 
             Assert.IsTrue(HashSet<string>.CreateSetComparer().Equals(set1, set2));
+        }
+
+        [Test]
+        public void TestSimple2()
+        {
+            Assert.AreEqual("0:00", this.util.ReadBinaryWatch(0)[0]);
+        }
+
+
+        [Test]
+        public void TestSimple3()
+        {
+            var expectedSet1 = new HashSet<string>
+                                   {
+                                       "1:00",
+                                       "2:00",
+                                       "4:00",
+                                       "8:00",
+                                       "0:01",
+                                       "0:02",
+                                       "0:04",
+                                       "0:08",
+                                       "0:16",
+                                       "0:32"
+                                   };
+
+            var actualSet = new HashSet<string>(this.util.ReadBinaryWatch(1));
+
+            Assert.IsTrue(HashSet<string>.CreateSetComparer().Equals(expectedSet1, actualSet));
         }
 
     }
