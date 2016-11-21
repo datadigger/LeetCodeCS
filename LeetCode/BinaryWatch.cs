ee2405fcbe7abc5e76081f1d87303563455103b3
yongqiang <jasonleehust@gmail.com>
2016-11-20 16:53:50 -0800

    Resolve problem of BinaryWatch.

---
+++ LeetCode/BinaryWatch.cs
@@ -0,0 +1,12 @@
+﻿namespace LeetCode
+{
+    using System.Collections.Generic;
+
+    public class BinaryWatch
+    {
+        public IList<string> ReadBinaryWatch(int num)
+        {
+            return null;
+        }
+    }
+}
---
+++ LeetCode/testing/BinaryWatchTest.cs
@@ -0,0 +1,24 @@
+﻿namespace LeetCode
+{
+    using System.Collections.Generic;
+
+    using NUnit.Framework;
+
+    [TestFixture]
+    public class BinaryWatchTest
+    {
+
+        private readonly BinaryWatch util = new BinaryWatch();
+
+        [Test]
+        public void TestSimple()
+        {
+            var set1 = new HashSet<string> { "1", "2" };
+
+            var set2 = new HashSet<string> { "2", "1" };
+
+            Assert.IsTrue(HashSet<string>.CreateSetComparer().Equals(set1, set2));
+        }
+
+    }
+}
