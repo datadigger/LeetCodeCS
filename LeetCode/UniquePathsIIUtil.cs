01b7fa9708aff171ec8c1595c267ee5cbb3ac8c5
Yongqiang Li <jasonleehust@gmail.com>
2016-11-21 08:58:54 -0800

    Resolve Problem of Unique Paths II

---
+++ LeetCode/UniquePathsIIUtil.cs
@@ -0,0 +1,46 @@
+﻿namespace LeetCode
+{
+    using System;
+
+    public class UniquePathsIIUtil
+    {
+        public int UniquePathsWithObstacles(int[,] obstacleGrid)
+        {
+            var m = obstacleGrid.GetUpperBound(0) + 1;
+            var n = obstacleGrid.GetUpperBound(1) + 1;
+
+            if (m == 0 || n == 0)
+            {
+                return 0;
+            }
+
+            var sum = new int[m, n];
+
+            for (var i = 0; i < m; ++i)
+            {
+                for (var j = 0; j < n; ++j)
+                {
+                    if (i - 1 >= 0 && j - 1 >= 0)
+                    {
+                        sum[i, j] = obstacleGrid[i, j] == 1 ? 0 : sum[i - 1, j] + sum[i, j - 1];
+                    }
+                    else if (i - 1 >= 0)
+                    {
+                        sum[i, j] = obstacleGrid[i, j] == 1 ? 0 : sum[i - 1, j];
+                    }
+                    else if (j - 1 >= 0)
+                    {
+                        sum[i, j] = obstacleGrid[i, j] == 1 ? 0 : sum[i, j - 1];
+                    }
+                    else
+                    {
+                        sum[i, j] = obstacleGrid[i, j] == 1 ? 0 : 1;
+                    }
+
+                }
+            }
+
+            return sum[m - 1, n - 1];
+        }
+    }
+}
---
+++ LeetCode/testing/UniquePathsIIUtilTest.cs
@@ -0,0 +1,34 @@
+﻿namespace LeetCode
+{
+    using NUnit.Framework;
+
+    [TestFixture]
+    public class UniquePathsIIUtilTest
+    {
+        private readonly UniquePathsIIUtil util = new UniquePathsIIUtil();
+
+        [Test]
+        public void TestSimple()
+        {
+            var obstacleGrid = new int[,] { { 0, 0, 0 }, { 0, 1, 0 }, { 0, 0, 0 }, };
+
+            Assert.That(2, Is.EqualTo(this.util.UniquePathsWithObstacles(obstacleGrid)));
+        }
+
+        [Test]
+        public void TestSimple2()
+        {
+            var obstacleGrid = new int[,] { { 0, 1}, {1, 0}};
+
+            Assert.That(0, Is.EqualTo(this.util.UniquePathsWithObstacles(obstacleGrid)));
+        }
+
+        [Test]
+        public void TestSimple3()
+        {
+            var obstacleGrid = new int[,] { { 1, 0} };
+
+            Assert.That(0, Is.EqualTo(this.util.UniquePathsWithObstacles(obstacleGrid)));
+        }
+    }
+}
