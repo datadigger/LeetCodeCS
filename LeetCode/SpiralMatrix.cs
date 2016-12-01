a4fdd24f6db0c109b55a7bb4cc76ee887270cd01
Yongqiang Li <jasonleehust@gmail.com>
2016-11-30 23:26:16 -0800

    Resolve the problem of Spiral Matrix

---
+++ LeetCode/SpiralMatrix.cs
@@ -0,0 +1,60 @@
+﻿namespace LeetCode
+{
+    using System;
+    using System.Collections.Generic;
+
+    public class SpiralMatrix
+    {
+        public IList<int> SpiralOrder(int[,] matrix)
+        {
+            var results = new List<int>();
+
+            if (matrix.Length == 0)
+            {
+                return results;
+            }
+
+            var w = matrix.GetUpperBound(0) + 1;
+            var h = matrix.GetUpperBound(1) + 1;
+
+            var used = new int[w, h];
+
+            var directions = new int[,] { { 0, 1 }, { 1, 0 }, { 0, -1 }, { -1, 0 } };
+
+            var currentDirection = 0;
+            var currentX = 0;
+            var currentY = 0;
+
+            var total = 0;
+
+            while (total < matrix.Length)
+            {
+                if (used[currentX, currentY] == 0)
+                {
+                    results.Add(matrix[currentX, currentY]);
+
+                    used[currentX, currentY] = 1;
+
+                    ++total;
+                }
+
+                var nextX = currentX + directions[currentDirection, 0];
+                var nextY = currentY + directions[currentDirection, 1];
+
+                if (nextX < 0 || nextX >= w || nextY < 0 || nextY >= h
+                 || used[nextX, nextY] == 1)
+                {
+                    currentDirection = (currentDirection + 1) % 4;
+                }
+                else
+                {
+                    currentX = nextX;
+                    currentY = nextY;
+                }
+
+            }
+
+            return results;
+        }
+    }
+}
---
+++ LeetCode/testing/SpiralMatrixTest.cs
@@ -0,0 +1,24 @@
+﻿namespace LeetCode
+{
+    using System.Linq;
+
+    using NUnit.Framework;
+
+    [TestFixture]
+    public class SpiralMatrixTest
+    {
+        private readonly  SpiralMatrix util = new SpiralMatrix();
+
+        [Test]
+        public void TestSimple()
+        {
+            var expect = new int[] { 1, 2, 3, 6, 9, 8, 7, 4, 5 };
+
+            var input = new int[,] { { 1, 2, 3 }, { 4, 5, 6 }, { 7, 8, 9 } };
+
+            var result = this.util.SpiralOrder(input);
+            Assert.AreEqual(expect, result.ToArray());
+        }
+
+    }
+}
